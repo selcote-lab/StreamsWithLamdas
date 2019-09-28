33f29ea1b4d6382304e70fae99a257d87c448fe4
zouhaire <zouhaire@idside.de>
2019-09-28 11:30:51 +0100

    used stream collect function

---
+++ src/com/tonasolution/Department.java
@@ -0,0 +1,22 @@
+package com.tonasolution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department {
+    private String name;
+    private List<Employee> employees;
+
+    public Department(String name) {
+        this.name = name;
+        this.employees = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee employee){
+        this.employees.add(employee);
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+}
--- src/com/tonasolution/Main.java
+++ src/com/tonasolution/Main.java
@@ -1,8 +1,12 @@
 package com.tonasolution;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -39,5 +43,60 @@ public class Main {
         System.out.println("-------------------------------------");
 //        System.out.println(concatStream.distinct().count());
         System.out.println(concatStream.distinct().peek(System.out::println).count());
+
+        Employee zouhaire = new Employee("Zouhaire Elakioui",29);
+        Employee ali = new Employee("Ali Sako",28);
+        Employee sarah = new Employee("Sarah Bamako",28);
+
+        Department hr = new Department("Humain Resources");
+        hr.addEmployee(ali);
+        hr.addEmployee(sarah);
+        Department accounting = new Department("Accounting");
+        accounting.addEmployee(zouhaire);
+
+        List<Department> departments = new ArrayList<>();
+        departments.add(hr);
+        departments.add(accounting);
+
+        departments
+                .stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .forEach(System.out::println);
+
+       /* List<String> sortedGNumbers = someBingNumbers
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .collect(Collectors.toList());*/
+        List<String> sortedGNumbers = someBingNumbers
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+
+        for (String s: sortedGNumbers){
+            System.out.println(s);
+        }
+
+        Map<Integer, List<Employee>> groupedByAge = departments.stream()
+                                                        .flatMap(department -> department.getEmployees().stream())
+                                                        .collect(Collectors.groupingBy( employee -> employee.getAge()));
+
+        departments.stream()
+                    .flatMap(department -> department.getEmployees().stream())
+                    .reduce((e1, e2) -> e1.getAge() < e2.getAge() ? e1 : e2)
+                    .ifPresent(System.out::println);
+
+        Stream.of("ABC", "AC", "BAA", "CCC", "XY", "ST")
+                .filter(s -> {
+                        System.out.println(s);
+                        return args.length == 3;
+                    }
+                )
+                .count();
+
+
     }
 }
